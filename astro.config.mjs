a7a98ce853d0da232357c02e9217deeb7b8ec0d7
Murat Alpay <muratalpay@MacBook-Pro.local>
2025-12-05 16:17:50 +0300

    Orijinal tasarıma dön + direkt profile redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  adapter: cloudflare(),
+  adapter: vercel(),
   integrations: [react(), sitemap()],
   output: 'server',
-  site: 'https://opn.bio',
+  site: 'https://opn-bio-site.vercel.app',
   devToolbar: {
     enabled: false,
   },
